d47eaa136caabf997a09049a89f81f2fd0342220
Ádám László <adam.laszlo.91@gmail.com>
2024-10-15 09:05:25 +0200

    Add emoji support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from typing import Any, Iterator, Mapping
 import git
 import ollama
@@ -11,23 +12,29 @@ def get_commit_messages(repo: git.Repo, active_branch: str, target_branch: str) 
 
 
 # TODO: Make ollama url a parameter
-# TODO: Add help
 # TODO: Add some error handling
-# TODO: Add emoji support
 
-def generate_description(commit_messages: list) -> Iterator[Mapping[str, Any]]:
-    # commit_message_str = '\n========\n'.join(commit_messages)
-    # TODO: Remove
-    commit_message_str = 'Add reference for food\n========\nFix memory error for plane 4\n========\nRemove unused code'
+def generate_description(commit_messages: list, emoji: bool) -> Iterator[Mapping[str, Any]]:
+    emoji_prompt = ' Use emojis.' if emoji else ''
+    commit_message_str = '\n========\n'.join(commit_messages)
     return ollama.chat(
         model='llama3.1',
-        messages=[{'role': 'system', 'content': 'You are a merge request description generator. You are given commit messages and you answer only with the generated description. Be concise.'},
+        options={
+            'temperature': 0
+        },
+        messages=[{'role': 'system', 'content': f'You are a merge request description generator. You are given commit messages and you answer only with the generated description. Be concise.{emoji_prompt}'},
                   {'role': 'user', 'content': commit_message_str}],
         stream=True,
     )
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog='merge-request-description-generator')
+    parser.add_argument('-e', '--emoji',
+                        action='store_true')
+    args = parser.parse_args()
+
     repo = git.Repo('.')
     active_branch = repo.active_branch.name
     # TODO: Make target branch a parameter
@@ -38,7 +45,7 @@ def main():
     print()
     commit_messages = get_commit_messages(
         repo=repo, active_branch=active_branch, target_branch=target_branch)
-    for chunk in generate_description(commit_messages=commit_messages):
+    for chunk in generate_description(commit_messages=commit_messages, emoji=args.emoji):
         print(chunk['message']['content'], end='', flush=True)
     print()
     print('=' * 32)
